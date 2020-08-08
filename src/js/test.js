01dc7778ab4390ece1b23e6652aacdcca06ca0e3
Maksim <makcxx20@gmail.com>
2020-08-08 20:10:53 +0300

    deleting borders

--- src/js/test.js
+++ src/js/test.js
@@ -11,112 +11,134 @@ let square = document.querySelector('.square'),
 
 
 function setActions(element) {
+
     let limits = {
-      top: canvas.offsetTop + 125,
-      right: canvas.offsetWidth + canvas.offsetLeft - element.offsetWidth / 2,
-      bottom: canvas.offsetHeight + canvas.offsetTop - element.offsetHeight / 2,
-      left: canvas.offsetLeft - 150
+      	top: canvas.offsetTop + 125,
+      	right: canvas.offsetWidth + canvas.offsetLeft - element.offsetWidth / 2,
+      	bottom: canvas.offsetHeight + canvas.offsetTop - element.offsetHeight / 2,
+      	left: canvas.offsetLeft - 150
     };
-element.onmousedown = function(e) {
+	element.onmousedown = function(e) {
+		let coords = getCoords(element);
+		let shiftX = e.pageX - coords.left;
+		let shiftY = e.pageY - coords.top;
 
-let coords = getCoords(element);
-let shiftX = e.pageX - coords.left;
-let shiftY = e.pageY - coords.top;
+		element.style.position = 'absolute';
+		if (element.classList.contains('square')) {
+			document.body.appendChild(square);
+		} else if (element.classList.contains('round')) {
+			document.body.appendChild(round);
+		}
+	/* document.body.appendChild(square); */
+		moveAt(e);
 
-element.style.position = 'absolute';
-if (element.classList.contains('square')) {
-    document.body.appendChild(square);
-    } else if (element.classList.contains('round')) {
-        document.body.appendChild(round);
-    }
-/* document.body.appendChild(square); */
-moveAt(e);
+		element.style.zIndex = 20; // над другими элементами
 
-element.style.zIndex = 20; // над другими элементами
+		function moveAt(e) {
+			element.style.left = e.pageX - shiftX + 'px';
+			element.style.top = e.pageY - shiftY + 'px';
 
-function moveAt(e) {
-  element.style.left = e.pageX - shiftX + 'px';
-  element.style.top = e.pageY - shiftY + 'px';
-  let newLocation = {
-    x: limits.left,
-    y: limits.top
-  };
-  if (e.pageX > limits.right) {
-    newLocation.x = limits.right;
-  } else if (e.pageX > limits.left) {
-    newLocation.x = e.pageX;
-  }
-  if (e.pageY > limits.bottom) {
-    newLocation.y = limits.bottom;
-  } else if (e.pageY > limits.top) {
-    newLocation.y = e.pageY;
-  }
-  relocate(newLocation);
-  function relocate(newLocation) {
-    element.style.left = newLocation.x - element.offsetWidth / 2 + 'px'; // попробовать отступ сюда тыкнуть
-    element.style.top = newLocation.y - element.offsetHeight /2 + 'px'; // попробовать отступ сюда тыкнуть
-  }
-}
+	/*   element.classList.add('active'); */
 
-document.onmousemove = function(e) {
-moveAt(e);
-};
+			let newLocation = {
+				x: limits.left,
+				y: limits.top
+			};
+			if (e.pageX > limits.right && e.pageX < limits.right + 75) {
+				newLocation.x = limits.right;
+			} else if (e.pageX > limits.left) {
+				newLocation.x = e.pageX;
+			}
+			if (e.pageY > limits.bottom && e.pageY < limits.bottom + 75) {
+				newLocation.y = limits.bottom;
+			} else if (e.pageY > limits.top) {
+				newLocation.y = e.pageY;
+			}
+			relocate(newLocation);
+			function relocate(newLocation) {
+				element.style.left = newLocation.x - element.offsetWidth / 2 + 'px'; // попробовать отступ сюда тыкнуть
+				element.style.top = newLocation.y - element.offsetHeight /2 + 'px'; // попробовать отступ сюда тыкнуть
+				
+			}
+		}
 
-element.onmouseup = function() {
-/* document.onmousemove = null;
-element.onmouseup = null; */
-let copy = document.createElement('div');
-if (element.classList.contains('square')) {
-copy.classList.add('square');
-element.classList.add('copy');
-} else if (element.classList.contains('round')) {
-copy.classList.add('round');
-element.classList.add('copy');
-}
-copy.classList.add("draggable");
-copy.classList.add("figure");
-copy.classList.add("copy");
-figures.append(copy);
-setActions(copy);
-document.onmousemove = null;
-copy.onmouseup = null;
-};
+		document.onmousemove = function(e) {
+			moveAt(e);
+		};
 
-};
+		function setActive(e) {
+		e.classList.add('active');
+		}
 
-element.ondragstart = function() {
-return false;
-};
+		element.onmouseup = function() {
+			let copy = document.createElement('div');
+			if (element.classList.contains('square')) {
+				copy.classList.add('square');
+				element.classList.add('copy');
+			} else if (element.classList.contains('round')) {
+				copy.classList.add('round');
+				element.classList.add('copy');
+			}
+			copy.classList.add("draggable");
+			copy.classList.add("figure");
+			copy.classList.add("copy");
+			figures.append(copy);
+			setActions(copy);
+
+			if (event.clientX > limits.right) {
+				figures.removeChild(element);
+			} else if(event.clientY > limits.bottom) {
+				figures.removeChild(element);
+			}
+
+			document.onmousemove = null;
+			copy.onmouseup = null;
+		};
+
+	};
+
+	element.ondragstart = function() {
+		return false;
+	};
 }
 setActions(square);
 setActions(round);
 function getCoords(elem) {
-let box = elem.getBoundingClientRect();
-return {
-top: box.top + pageYOffset,
-left: box.left + pageXOffset
-};
+	let box = elem.getBoundingClientRect();
+	return {
+		top: box.top + pageYOffset,
+		left: box.left + pageXOffset
+	};
+
 }
 
 document.addEventListener('click', function(e) {
-    const items = document.querySelectorAll('.draggable');
-    const target = e.target;
-  Array.from(items).forEach(item => {
-      item.classList.remove('active');
-      item.style.zIndex = 5;
-  });
-  if(event.target.classList.contains('figure')) {
-    event.target.classList.add('active');
-    event.target.style.zIndex = 200;
-  } else if (event.target == first){
-    first.classList.add('active');
-  }
+  	const items = document.querySelectorAll('.draggable');
+	Array.from(items).forEach(item => {
+    	item.classList.remove('active');
+    	item.style.zIndex = 5;
+	});
+	if(event.target.classList.contains('draggable')) {
+		event.target.classList.add('active');
+		event.target.style.zIndex = 200;
+	}
 });
 
+function disableActive(e) {
+  	e.classList.remove('active');
+}
+
+/* let elt = document.getElementById('first');
+
+elt.addEventListener('click', function(e) {
+    if (e.target.classList.contains('draggable')) {
+      e.target.classList.add('active');
+    }
+}); */
+
 document.addEventListener('keyup', function(event) {
-  let active = document.querySelector('.active');
-  if (event.keyCode == 46) {
-    let oldChild = figures.removeChild(active);
-    figures.removeChild(active);
-  }
+  	let active = document.querySelector('.active');
+  	if (event.keyCode == 46) {
+    	figures.removeChild(active);
+  	}
 });
